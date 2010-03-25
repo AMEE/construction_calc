bfc5de599946a03b9a9c1cfb79c7be2fcfc8ffa5
George Palmer <george.palmer@gmail.com>
2010-03-25 13:30:03 +0000

    Fix units helper for alternative units

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,7 +10,7 @@ module ApplicationHelper
   
   def units_name_from_amee_unit(carbon_producing_type)
     unit_obj = Unit.from_amee_unit(carbon_producing_type.units)
-    unit_obj ? unit_obj.name : convert_units_to_readable_format(carbon_producing_type.units)
+    unit_obj ? unit_obj.name : carbon_producing_type.units
   end
   
   def two_decimal_place_float(amount)
