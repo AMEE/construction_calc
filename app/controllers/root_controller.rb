006c32dcb6d9f5576944b09ed12e10f2f257af02
Andrew Hill <andrew.hill@amee.com>
2010-06-14 13:52:23 +0100

    Cross-merged improvements to the initial welcome message shown to the admin user on first login (MA-157) from the Monthly Calculator into the Construction Calculator. References MA-204.

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -7,11 +7,20 @@ class RootController < ApplicationController
     if projects.size > 0
       redirect_to project_path(projects.first)
     elsif current_user.admin? || current_user.client_admin?(@client)
-      flash[:error] = "There are no projects that you have permissions to read. Create your first construction project here."
+      flash[:info] = [
+        "Welcome to the Construction Calculator!",
+        "&nbsp;",
+        "The Construction Calculator allows you to create a number of
+        construction projects, each with their own set of users that can log in
+        and either view and/or update the data stored.",
+        "&nbsp;",
+        "To get started, you need to create a new construction project. Please
+        enter a new project name below, and the start date for the project."
+      ]
       redirect_to new_project_path
     else
       logout_killing_session!
-      flash[:error] = "You don't have access to any projects.  Contact your administrator"
+      flash[:error] = "You don't have access to any projects. Contact your administrator."
       redirect_to login_path
     end
   end
