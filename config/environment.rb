ad03286a63f0fa558a28fb1a7f55a08304ffb9a8
Andrew Hill <andrew.hill@amee.com>
2010-06-23 15:25:16 +0100

    Added optional hoptoad support - MA-220.

--- config/environment.rb
+++ config/environment.rb
@@ -22,6 +22,8 @@ Rails::Initializer.run do |config|
   config.gem "will_paginate"
   config.gem 'prawn', :version => "0.7.1"
 
+  config.gem 'hoptoad_notifier'
+
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
